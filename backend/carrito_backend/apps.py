1b991738c3d613a242c1d2a05545d5063742095f
Bruno <brunobonil@gmail.com>
2023-04-18 01:04:13 -0300

    Primer commit

---
+++ backend/carrito_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarritoBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carrito_backend'
---
+++ backend/carrito_backend/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Producto(models.Model):
+    nombre=models.CharField(max_length=60)
+    descripcion=models.CharField(max_length=300)
+    precio=models.FloatField()
+    codigo_ean=models.IntegerField()
+
+    def __str__(self):
+        return (self.nombre)
+
+class Carrito(models.Model):
+    fecha=models.DateField()
+
+    @property
+    def precioTotal(self):
+        pass
+
+class DetalleCarrito(models.Model):
+    id_carrito=models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    id_producto=models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad=models.IntegerField()        
+
+    @property
+    def subtotal(self):
+        return self.cantidad * self.productos_set.all()
+
+    
